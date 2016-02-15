f1580173fc9ccc3489e20bee19cfcf998f1092a1
skohar <skohar@users.noreply.github.com>
2016-02-15 16:57:59 +0900

    Remove original sns model

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,12 @@ aws lambda update-function-code \
   --function-name $FUNCTION_NAME \
   --zip-file fileb://target/scala-2.11/$projectName-assembly-0.1-SNAPSHOT.jar
 
-fileUrl="fileb://target/scala-2.11/${projectName}-assembly-0.1-SNAPSHOT.jar"
-aws lambda create-function \
-  --function-name $FUNCTION_NAME \
-  --zip-file $fileUrl \
-  --role arn:aws:iam::$AWS_ID:role/$LAMBDA_ROLE \
-  --handler $LAMBDA_HANDLER \
-  --runtime java8 \
-  --timeout 30 \
-  --memory-size 512
+#fileUrl="fileb://target/scala-2.11/${projectName}-assembly-0.1-SNAPSHOT.jar"
+#aws lambda create-function \
+#  --function-name $FUNCTION_NAME \
+#  --zip-file $fileUrl \
+#  --role arn:aws:iam::$AWS_ID:role/$LAMBDA_ROLE \
+#  --handler $LAMBDA_HANDLER \
+#  --runtime java8 \
+#  --timeout 30 \
+#  --memory-size 512
