cc0dd77c6def5356f32a8ea94c1fb16989ac65de
shokohara <shokohara@users.noreply.github.com>
2016-08-10 17:01:04 +0900

    .

--- src/main/scala/com/github/skohar/notifytoslack/App.scala
+++ src/main/scala/com/github/skohar/notifytoslack/App.scala
@@ -16,35 +16,38 @@ import scalaz._
 
 class App {
 
-  def toMessage(sns: SNS) = Json.parse(sns.getMessage).as[Message]
+//  def toMessage(sns: SNS) = Json.parse(sns.getMessage).as[Message]
 
   val Alarm = "ALARM"
   val InsufficientData = "INSUFFICIENT_DATA"
   val Ok = "OK"
 
   def handler(event: SNSEvent, context: Context): String = {
+    context.getLogger.log("helpme")
     App.description2config(context).leftMap(ExceptionUtils.getStackTrace).leftMap(context.getLogger.log)
     (for {
       config <- App.description2config(context).leftMap(ExceptionUtils.getStackTrace)
     } yield try {
+      context.getLogger.log("yay1")
       event.getRecords.map(_.getSNS).map(_.getMessage).foreach(context.getLogger.log)
       context.getLogger.log("yay2")
-      val messages = event.getRecords.map(_.getSNS).map(toMessage).filter(_.NewStateReason === Alarm).map { sns =>
-        s""":exclamation: * ${sns.NewStateValue} : ${sns.AlarmDescription}*
-            |${sns.NewStateReason}""".stripMargin
-      } ++ event.getRecords.map(_.getSNS).map(toMessage).filter(_.NewStateReason === InsufficientData).map { sns =>
-        s""":warning: * ${sns.NewStateValue} : ${sns.AlarmDescription}*
-            |${sns.NewStateReason}""".stripMargin
-      } ++ event.getRecords.map(_.getSNS).map(toMessage).filter(_.NewStateReason === Ok).map { sns =>
-        s""":+1: * ${sns.NewStateValue} : ${sns.AlarmDescription}*
-            |${sns.NewStateReason}""".stripMargin
-      }
-      context.getLogger.log(config.toString)
-      messages.foreach(context.getLogger.log)
-      messages.foreach { message =>
-        new SlackApi(s"https://${config.slackWebHookUrl}").call(new SlackMessage("CloudWatch", message))
-      }
-      messages.mkString
+//      val messages = event.getRecords.map(_.getSNS).map(toMessage).filter(_.NewStateReason === Alarm).map { sns =>
+//        s""":exclamation: * ${sns.NewStateValue} : ${sns.AlarmDescription}*
+//            |${sns.NewStateReason}""".stripMargin
+//      } ++ event.getRecords.map(_.getSNS).map(toMessage).filter(_.NewStateReason === InsufficientData).map { sns =>
+//        s""":warning: * ${sns.NewStateValue} : ${sns.AlarmDescription}*
+//            |${sns.NewStateReason}""".stripMargin
+//      } ++ event.getRecords.map(_.getSNS).map(toMessage).filter(_.NewStateReason === Ok).map { sns =>
+//        s""":+1: * ${sns.NewStateValue} : ${sns.AlarmDescription}*
+//            |${sns.NewStateReason}""".stripMargin
+//      }
+//      context.getLogger.log(config.toString)
+//      messages.foreach(context.getLogger.log)
+//      messages.foreach { message =>
+//        new SlackApi(s"https://${config.slackWebHookUrl}").call(new SlackMessage("CloudWatch", message))
+//      }
+//      messages.mkString
+      ""
     } catch {
       case t: Throwable =>
         val stackTraceString = ExceptionUtils.getStackTrace(t)
