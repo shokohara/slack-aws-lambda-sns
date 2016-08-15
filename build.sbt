a2aedd400ceca8bf61abb3f0d73b683def6d87ed
shokohara <shokohara@users.noreply.github.com>
2016-08-15 14:33:05 +0900

    .

--- build.sbt
+++ build.sbt
@@ -3,12 +3,12 @@ name := "notify-to-slack"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "com.amazonaws" % "aws-java-sdk-lambda" % "1.11.+",
-  "com.amazonaws" % "aws-lambda-java-core" % "1.1.+",
-  "com.amazonaws" % "aws-lambda-java-events" % "1.1.+",
+  "com.amazonaws" % "aws-java-sdk-lambda" % "1.11.26",
+  "com.amazonaws" % "aws-lambda-java-core" % "1.1.0",
+  "com.amazonaws" % "aws-lambda-java-events" % "1.1.0",
   "com.typesafe.play" %% "play-json" % "2.5.4",
   "com.github.tototoshi" %% "play-json-naming" % "1.1.0",
-  "net.gpedro.integrations.slack" % "slack-webhook" % "1.1.1",
+  "net.gpedro.integrations.slack" % "slack-webhook" % "1.2.0",
   "org.typelevel" %% "cats" % "0.4.1",
   "org.scalaz" %% "scalaz-core" % "7.2.5",
   "org.apache.commons" % "commons-lang3" % "3.4",
--- src/main/scala/com/github/skohar/notifytoslack/App.scala
+++ src/main/scala/com/github/skohar/notifytoslack/App.scala
@@ -5,6 +5,7 @@ import com.amazonaws.services.lambda.model.GetFunctionRequest
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.SNSEvent
 import com.amazonaws.services.lambda.runtime.events.SNSEvent.SNS
+import com.fasterxml.jackson.core.JsonParseException
 import net.gpedro.integrations.slack.{SlackApi, SlackMessage}
 import org.apache.commons.lang3.exception.ExceptionUtils
 import play.api.data.validation.ValidationError
@@ -24,42 +25,36 @@ import syntax.bind._
 
 class App {
 
-  def toMessage(sns: SNS): Seq[(JsPath, Seq[ValidationError])] \/ Message =
-    \/.fromEither(Json.parse(sns.getMessage).validate[Message].asEither)
-
-  val Alarm = "ALARM"
-  val InsufficientData = "INSUFFICIENT_DATA"
-  val Ok = "OK"
+  def toMessage(sns: SNS): JsResultException \/ Message = try {
+    \/-(Json.parse(sns.getMessage).as[Message])
+  } catch {
+    case e: (_:JsResultException | _:JsonParseException)=> -\/(e)
+  }
 
   def handler(event: SNSEvent, context: Context): String = {
-    context.getLogger.log("helpme")
-    App.description2config(context).leftMap(ExceptionUtils.getStackTrace).leftMap(context.getLogger.log)
     (for {
       config <- App.description2config(context).leftMap(ExceptionUtils.getStackTrace)
     } yield try {
-      context.getLogger.log("yay1")
-      event.getRecords.map(_.getSNS).map(_.getMessage).foreach(context.getLogger.log)
-      context.getLogger.log("yay2")
+      event.getRecords.map(_.getSNS).map(_.getMessage).foreach(App.log(config, _))
       val emoji = (_: String) match {
-        case Alarm => "exclamation"
-        case InsufficientData => "warning"
-        case Ok => "+1"
+        case "ALARM" => "exclamation"
+        case "INSUFFICIENT_DATA" => "warning"
+        case "OK" => "+1"
       }
-      def toText(x: Message) =
-        s""":${emoji(x.NewStateReason)}: * ${x.NewStateValue} : ${x.AlarmDescription}*
-            |${x.NewStateReason}""".stripMargin
-      val messages: Seq[String] = event.getRecords.map(_.getSNS).map(toMessage).map {
-        case -\/(x) => s"""``` ${ExceptionUtils.getStackTrace(JsResultException(x))} ```"""
+      def toText(message: Message) =
+        s""":${emoji(message.NewStateReason)}: * ${message.NewStateValue} : ${message.AlarmDescription}*
+            |${message.NewStateReason}""".stripMargin
+      val messages = event.getRecords.map(_.getSNS).map(toMessage).map {
+        case -\/(x) => s"""``` ${ExceptionUtils.getStackTrace(x)} ```"""
         case \/-(x) => toText(x)
       }
-      messages.foreach { message =>
-        new SlackApi(s"https://${config.slackWebHookUrl}").call(new SlackMessage("CloudWatch", message))
-      }
+      messages.map(new SlackMessage("CloudWatch", _)).foreach(new SlackApi(s"https://${config.slackWebHookUrl}").call)
       messages.mkString
+      ""
     } catch {
       case t: Throwable =>
         val stackTraceString = ExceptionUtils.getStackTrace(t)
-        context.getLogger.log(stackTraceString)
+        App.log(config, stackTraceString)
         stackTraceString
     }).toString
   }
@@ -71,8 +66,8 @@ object App {
     new SlackApi(config.slackWebHookUrl).call(new SlackMessage("lambda:debug", s"``` $text ```"))
 
   def description2config(context: Context) = \/.fromEither(allCatch either {
-    val description = new AWSLambdaClient()
-      .getFunction(new GetFunctionRequest().withFunctionName(context.getFunctionName)).getConfiguration.getDescription
+    val request = new GetFunctionRequest().withFunctionName(context.getFunctionName)
+    val description = new AWSLambdaClient().getFunction(request).getConfiguration.getDescription
     Json.parse(description).as[LambdaConfig]
   })
 }
