6032ee50490707ddc0099fb841e853c5b32e8c4d
shokohara <shokohara@users.noreply.github.com>
2016-08-19 17:22:42 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,18 @@
 # LAMBDA_ROLE
 # LAMBDA_HANDLER
 projectName=$(sbt -no-colors name | tail -n1 | cut -d' ' -f2)
+projectVersion=$(sbt -no-colors version | tail -n1 | cut -d' ' -f2)
 
 sbt clean && \
 sbt assembly && \
-aws s3 cp target/scala-2.11/$projectName-assembly-0.1-SNAPSHOT.jar s3://$BUCKET_NAME/ && \
+aws s3 cp target/scala-2.11/$projectName-assembly-$projectVersion.jar s3://$BUCKET_NAME/ && \
 aws lambda update-function-code \
   --function-name $FUNCTION_NAME && \
 aws lambda update-function-code \
   --function-name $FUNCTION_NAME \
-  --s3-bucket $S3_BUCKET --s3-key $projectName-assembly-0.1-SNAPSHOT.jar
+  --s3-bucket $BUCKET_NAME --s3-key $projectName-assembly-$projectVersion.jar
 
-#fileUrl="fileb://target/scala-2.11/${projectName}-assembly-0.1-SNAPSHOT.jar"
+#fileUrl="fileb://target/scala-2.11/${projectName}-assembly-$projectVersion.jar"
 #aws lambda create-function \
 #  --function-name $FUNCTION_NAME \
 #  --zip-file $fileUrl \
